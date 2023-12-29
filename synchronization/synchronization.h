43f79a22a74cd59e681c18380bfe1d297bec3dcd
adnanemagourt <adnane.aitmagourt@outlook.com>
2023-12-29 22:45:03 +0100

    complete

--- synchronization/synchronization.h
+++
@@ -1,8 +0,0 @@
-#ifndef SYNCHRONIZATION_H
-#define SYNCHRONIZATION_H
-
-#include <string>
-
-void synchronizeDirectories(const std::string& sourceDir, const std::string& destDir);
-
-#endif // SYNCHRONIZATION_H
